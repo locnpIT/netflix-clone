9d420225dd70d7b8aaab44f99749791fd58a9096
locnpIT <npl2004qn@gmail.com>
2025-02-25 00:25:17 +0700

    upload

--- src/components/TitleCard/TitleCards.jsx
+++ src/components/TitleCard/TitleCards.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data';
 
@@ -10,7 +10,22 @@ import cards_data from '../../assets/cards/Cards_data';
 // eslint-disable-next-line react/prop-types
 const TitleCards = ({title, category}) => {
 
+    const [apiData, setApiData] = useState([]);
+
     const cardsRef = useRef();
+
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Yzk1MjBkZjNjZDVkMzAzM2E1M2Q5OGVkOWY0ZmIwMiIsIm5iZiI6MTc0MDQxNTE0Mi42MjcsInN1YiI6IjY3YmNhMGE2MjNmZTExN2VlNmE0NzI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3DU4wVVHy37LAGJ7Ju1X0s_V9Yohv7ZbPqMCLVIDwfg'
+        }
+      };
+      
+
+
 
     const handleWheel = (event) =>{
         event.preventDefault();
@@ -18,6 +33,10 @@ const TitleCards = ({title, category}) => {
     }
 
     useEffect(() =>{
+        fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+            .then(res => res.json())
+            .then(res => setApiData(res.results))
+            .catch(err => console.error(err));
         cardsRef.current.addEventListener('wheel', handleWheel);
     }, [])
 
@@ -28,10 +47,10 @@ const TitleCards = ({title, category}) => {
                 {title?title:"Popular on NetFlix"}
             </h2>
             <div className='card-list' ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return  <div className='card' key={index}>
-                        <img src={card.image} alt='' />
-                        <p>{card.name}</p> 
+                        <img src={`https://image.tmdb.org/t//p/w500/${card.backdrop_path}`} alt='' />
+                        <p>{card.original_title}</p> 
                     </div>
                 })}
             </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,10 +31,10 @@ const Home = () => {
             </div>
 
             <div className='more-cards'>
-                <TitleCards title={"Blockbuster Movies"}/>
-                <TitleCards title={"Only on Netflix"}/>
-                <TitleCards title={"Upcoming"}/>
-                <TitleCards title={"Topics for You"}/>
+                <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+                <TitleCards title={"Only on Netflix"} category={"popular"}/>
+                <TitleCards title={"Upcoming"} category={"upcoming"}/>
+                <TitleCards title={"Topics for You"} category={"now_playing"}/>
             </div>
             <Footer/>
         </div>
