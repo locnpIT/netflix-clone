358d63a03a7db09f7fc159dd4d3cffe69446f5f5
locnpIT <npl2004qn@gmail.com>
2025-02-24 23:19:12 +0700

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Home from './pages/Home/Home';
+// eslint-disable-next-line no-unused-vars
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
 
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      
     </div>
   );
 };
