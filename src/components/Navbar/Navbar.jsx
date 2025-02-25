b9edd809336b9679d6635c5407992f522eb319eb
locnpIT <npl2004qn@gmail.com>
2025-02-26 00:18:41 +0700

    upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable no-unused-vars */
 
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg';
@@ -9,8 +9,21 @@ import profile_img from '../../assets/profile_img.png';
 import caret_icon from '../../assets/caret_icon.svg';
 
 const Navbar = () => {
+
+    const navRef = useRef();
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY >= 80){
+                navRef.current.classList.add('nav-dark')
+            }else{
+                navRef.current.classList.remove('nav-dark')
+            }
+        })
+    }, []);
+
     return (
-        <div className='navbar'>
+        <div ref={navRef} className='navbar'>
             <div className='navbar-left'>
                 <img src={logo} alt='' />
                 <ul>
